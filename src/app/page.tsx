7d793ee9d49b0863b7c64e77e9e0957a8d963344
Ebrahimhanif12 <ebrahimhanif810@gmail.com>
2025-09-03 19:41:57 +0600

    trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { Client } from "./client";
+
+
+
 
 
 export default  async function Home() {
+ const queryClient = getQueryClient();
+ void queryClient.prefetchQuery(trpc.createAI.queryOptions({text : "Ebrahim PREFETCH"}))
+
  
 
 
   return (
-    <div>
-    Hello world
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading....</p>}>
+        <Client></Client>
+      </Suspense>
+    </HydrationBoundary>
   );
 }
