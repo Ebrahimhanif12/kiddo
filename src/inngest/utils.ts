183435d41139bdbcfe0f0cba2b3dfec09bf1582a
Ebrahimhanif12 <ebrahimhanif810@gmail.com>
2025-09-19 21:15:42 +0600

    Agent tools added

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,7 +1,21 @@
 import {Sandbox} from "@e2b/code-interpreter";
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
 export async function getSandbox(sanboxId: string){
     const sandbox = await Sandbox.connect(sanboxId);
     return sandbox;
+}
+
+export function lastAssistantTextMessageContent(result: AgentResult){
+    const lastAssistantTextMessageIndex = result.output.findLastIndex(
+        (message) => message.role === "assistant",
+    );
+
+    const message = result.output[lastAssistantTextMessageIndex] as | TextMessage | undefined;
+    return message?.content
+    ? typeof message.content === "string"
+        ? message.content
+        : message.content.map((c) => c.text).join("")
+    : undefined;
 }
 
