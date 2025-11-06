885307105016e2fca7ca3bc9f2a6ac9c367a3674
Ebrahimhanif12 <ebrahimhanif810@gmail.com>
2025-11-06 20:22:43 +0600

    project header done

---
+++ src/modules/projects/ui/components/message-loading.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import{useState, useEffect} from "react";
+
+const ShimmerMessage = () => {
+    const message = [
+        "Thinking...",
+        "Loading...",
+        "Kiddo is doing her best...",
+        "Analyzing Your Request...",
+        "Building your Website...",
+        "Crafting components...",
+        "Optimizing layout...",
+        "Adding final touches...",
+        "Almost Ready...",
+    ]
+
+    const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() =>{
+            setCurrentMessageIndex((prev) => (prev+1) % message.length);
+        },2000)
+    }, [message.length]);
+
+    return (
+        <div className="flex items-center gap-2">
+            <span className="text-base text-muted-foreground animate-pulse">
+                {message[currentMessageIndex]}
+            </span>
+        </div>
+    )
+}
+
+export const MessageLoading = () => {
+    return(
+        <div className="flex flex-col group px-2 pb-4">
+            <div className="flex items-center gap-2 pl-2 mb-2">
+                <Image 
+                src="/kiddoLogo.png"
+                alt="kiddo"
+                width={18}
+                height={18}
+                className="shrink-0"
+                />
+                <span className="text-sm font-medium">Kiddo</span>
+            </div>
+            <div className="pl-8.5 flex flex-col gap-y-4">
+                <ShimmerMessage/>
+            </div>
+        </div>
+    )
+}
+
+
--- src/modules/projects/ui/views/project-views.tsx
+++ src/modules/projects/ui/views/project-views.tsx
@@ -10,12 +10,15 @@ import {
 
 } from "@/components/ui/resizable"
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
+import { ProjectHeader } from "../components/project-header";
 
 interface Props {
     projectId: string;
 }
 export const ProjectView = ({ projectId }: Props) => {
+    const [activeFragment, setActiveFragment] = useState<Fragment| null>(null);
     // const trpc = useTRPC();
     // const { data: project } = useSuspenseQuery(
     //     trpc.project.getOne.queryOptions({
@@ -30,8 +33,18 @@ export const ProjectView = ({ projectId }: Props) => {
                     defaultSize={35}
                     minSize={20}
                     className="flex flex-col min-h-0">
+                    <Suspense fallback={<p>Loading project...</p>}>
+                    <ProjectHeader projectId={projectId} />
+                    </Suspense>
+                    
                     <Suspense fallback={<p>Loading....</p>}>
-                    <MessagesContainer projectId={projectId}></MessagesContainer>
+                    <MessagesContainer
+                     projectId={projectId}
+                     activeFragment= {activeFragment}
+                     setActiveFragment={setActiveFragment}
+
+
+                    ></MessagesContainer>
                     </Suspense>
 
 
