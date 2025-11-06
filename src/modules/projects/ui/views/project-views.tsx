29e8fbb8e16a9cc11996a60331c60f5539e32e5c
Ebrahimhanif12 <ebrahimhanif810@gmail.com>
2025-11-07 01:51:33 +0600

    code view done

--- src/modules/projects/ui/views/project-views.tsx
+++ src/modules/projects/ui/views/project-views.tsx
@@ -14,17 +14,25 @@ import { Suspense, useState } from "react";
 import { Fragment } from "@/generated/prisma";
 import { ProjectHeader } from "../components/project-header";
 import { FragmentWeb } from "../components/fragment-web";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { CodeIcon, CrownIcon, EyeIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { CodeView } from "@/components/code-view";
+import { FileExplorer } from "@/components/file-explorer";
 
 interface Props {
     projectId: string;
 }
 export const ProjectView = ({ projectId }: Props) => {
-    const [activeFragment, setActiveFragment] = useState<Fragment| null>(null);
+    const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+    const [tabstate, setTabState] = useState<"preview" | "code">("preview");
     // const trpc = useTRPC();
     // const { data: project } = useSuspenseQuery(
     //     trpc.project.getOne.queryOptions({
     //         id: projectId,
     //     }))
+
 
 
     return (
@@ -35,17 +43,17 @@ export const ProjectView = ({ projectId }: Props) => {
                     minSize={20}
                     className="flex flex-col min-h-0">
                     <Suspense fallback={<p>Loading project...</p>}>
-                    <ProjectHeader projectId={projectId} />
+                        <ProjectHeader projectId={projectId} />
                     </Suspense>
-                    
+
                     <Suspense fallback={<p>Loading....</p>}>
-                    <MessagesContainer
-                     projectId={projectId}
-                     activeFragment= {activeFragment}
-                     setActiveFragment={setActiveFragment}
+                        <MessagesContainer
+                            projectId={projectId}
+                            activeFragment={activeFragment}
+                            setActiveFragment={setActiveFragment}
 
 
-                    ></MessagesContainer>
+                        ></MessagesContainer>
                     </Suspense>
 
 
@@ -55,9 +63,45 @@ export const ProjectView = ({ projectId }: Props) => {
                     defaultSize={65}
                     minSize={50}
                 >
-                    {!!activeFragment && <FragmentWeb data={activeFragment}/>}
+                    <Tabs
+                        className="h-full gap-y-0"
+                        defaultValue="preview"
+                        value={tabstate}
+                        onValueChange={(value) => setTabState(value as "preview" | "code")}
+
+                    >
+
+                        <div className="w-full flex items-center p-2 border-b gap-x-2">
+                            <TabsList className="h-8 p-0 border rounded-m">
+                                <TabsTrigger value="preview" className="rounded-md">
+                                    <EyeIcon/> <span>Demo</span>
+                                </TabsTrigger>
+
+                                 <TabsTrigger value="code" className="rounded-md">
+                                    <CodeIcon/> <span>Code</span>
+                                </TabsTrigger>
+                            </TabsList>
+                            <div className="ml-auto flex items-center gap-x-2">
+                                <Button asChild size="sm" variant="default">
+                                    <Link href="/pricing">
+                                    <CrownIcon/>Upgrade
+                                    </Link>
+                                </Button>
+                            </div>
+                        </div>
+                        <TabsContent value="preview">
+                            {!!activeFragment && <FragmentWeb data={activeFragment} />}
+                        </TabsContent>
+                        <TabsContent value="code" className="min-h-0">
+                            {!!activeFragment?.files && (
+                                <FileExplorer 
+                                files={activeFragment.files as {[path: string]: string}}
+                                />
+                            )}
+                        </TabsContent>
+                    </Tabs>
                 </ResizablePanel>
- 
+
             </ResizablePanelGroup>
 
 
